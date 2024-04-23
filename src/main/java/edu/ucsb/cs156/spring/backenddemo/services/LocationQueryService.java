db7c86f621a74e0b438b806266d5feb9f58b1948
james-y-f <jamesfuyuanhang@gmail.com>
2024-04-23 15:50:48 -0700

    jf - location query service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
@@ -1,14 +1,54 @@
+// package edu.ucsb.cs156.spring.backenddemo.services;
+
+
+// import org.springframework.boot.web.client.RestTemplateBuilder;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.client.HttpClientErrorException;
+// import org.springframework.web.client.RestTemplate;
+
+
+// @Service
+// public class LocationQueryService {
+
+//     private final RestTemplate restTemplate;
+
+//     public LocationQueryService(RestTemplateBuilder restTemplateBuilder) {
+//         restTemplate = restTemplateBuilder.build();
+//     }
+
+//     public static final String ENDPOINT = 
+//       "https://nominatim.openstreetmap.org/search?q={location}&format=jsonv2";
+
+//     public String getJSON(String location) throws HttpClientErrorException {
+//         return "";
+//     }
+// }
+
 package edu.ucsb.cs156.spring.backenddemo.services;
 
+import java.util.List;
+import java.util.Map;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+
+@Slf4j
 @Service
 public class LocationQueryService {
+
+    ObjectMapper mapper = new ObjectMapper();
 
     private final RestTemplate restTemplate;
 
@@ -17,9 +57,23 @@ public class LocationQueryService {
     }
 
     public static final String ENDPOINT = 
-      "https://nominatim.openstreetmap.org/search?q={location}&format=jsonv2";
+        "https://nominatim.openstreetmap.org/search?q={location}&format=jsonv2";
 
-    public String getJSON(String location) throws HttpClientErrorException {
-        return "";
+        public String getJSON(String location) throws HttpClientErrorException {
+        log.info("location={}", location);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        Map<String, String> uriVariables = Map.of("location", location);
+
+        ResponseEntity<String> re = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class,
+                uriVariables);
+        return re.getBody();
     }
+
+   
+
 }
