746a26bace01562bb2a7cdffbaba1e690d53ff26
james-y-f <jamesfuyuanhang@gmail.com>
2024-04-23 16:24:49 -0700

    jf - cleaned up code

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
@@ -1,29 +1,3 @@
-// package edu.ucsb.cs156.spring.backenddemo.services;
-
-
-// import org.springframework.boot.web.client.RestTemplateBuilder;
-// import org.springframework.stereotype.Service;
-// import org.springframework.web.client.HttpClientErrorException;
-// import org.springframework.web.client.RestTemplate;
-
-
-// @Service
-// public class LocationQueryService {
-
-//     private final RestTemplate restTemplate;
-
-//     public LocationQueryService(RestTemplateBuilder restTemplateBuilder) {
-//         restTemplate = restTemplateBuilder.build();
-//     }
-
-//     public static final String ENDPOINT = 
-//       "https://nominatim.openstreetmap.org/search?q={location}&format=jsonv2";
-
-//     public String getJSON(String location) throws HttpClientErrorException {
-//         return "";
-//     }
-// }
-
 package edu.ucsb.cs156.spring.backenddemo.services;
 
 import java.util.List;
